70aaff601e3ae6dcff6d00a792987cba58a7d61c
bryanoli <bryan.olivarez14@gmail.com>
2023-08-21 16:52:14 -0700

    Initial commit

---
+++ CameraDemo/app/src/main/java/edu/ucsb/ece150/demos/camerademo/MainActivity.java
@@ -0,0 +1,43 @@
+package edu.ucsb.ece150.demos.camerademo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      }
+
+    //onClick method 'takePicture' coming from button click
+    public void takePicture(View view) {
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        //Implicit Intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //The Android Camera application encodes the photo in the return Intent
+    //delivered to onActivityResult() as a Bitmap in the extras, under the key "data"
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
